2a127976cb6ec2c61ba0fbdc8a116a3657cdcedc
Vivek <kingdragon46@gmail.com>
2021-04-17 16:54:02 +0530

    Added form

--- Aaghaz/urls.py
+++ Aaghaz/urls.py
@@ -18,9 +18,9 @@ from django.urls import path, include  # add this
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + [
+urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("Authenticate.urls")), # Auth routes - login / register
     path("", include("Website.urls")), # Website routes
     path("master/", include("Master.urls")), # Master routes
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
