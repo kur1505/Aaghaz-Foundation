d1a405c21d7488ac7a9f7e3d69716f1f02f233e0
Vivek <kingdragon46@gmail.com>
2021-04-17 16:42:07 +0530

    Added form

--- Aaghaz/settings.py
+++ Aaghaz/settings.py
@@ -13,7 +13,7 @@ import os
 from decouple import config
 from unipath import Path
 import dj_database_url
-from pathlib import Path
+# from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).parent
@@ -123,7 +123,7 @@ WSGI_APPLICATION = 'Aaghaz.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'db.sqlite3',
     }
 }
 # Added database
